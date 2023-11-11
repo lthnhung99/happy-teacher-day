677ad6ce220b94fcb5f71b7c39084bad3de68077
Hồng Nhung <lenhung14101999@gmail.com>
2023-11-11 20:34:59 +0700

    20/11

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = "";
+const giftUrl = "http://nodemy.vn";
+const eventName = "Chúc Mừng ngày nhà giáo Việt Nam 20-11";
+const titleCard = "Gửi các anh giảng viên Codegym Huế";
+const contentCard =
+  "Nhân ngày 20/11, chúng em xin gửi đến các anh giảng viên những lời tri ân tốt đẹp nhất. Xin kính chúc các anh giảng viên thật mạnh khỏe để mãi là người chỉ đường gần gũi và yêu thương nhất đối với mọi học viên… 💘";
+const giftImage = "IMG_7349.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
